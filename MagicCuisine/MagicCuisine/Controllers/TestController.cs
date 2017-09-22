eb4dbb9a0b811469e1109ab4ebcb4cda50016b95
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-09-22 17:19:18 +0300

    init ninject container

---
+++ MagicCuisine/MagicCuisine/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MagicCuisine.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IDataBase db;
+
+        public TestController(IDataBase db)
+        {
+            this.db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
