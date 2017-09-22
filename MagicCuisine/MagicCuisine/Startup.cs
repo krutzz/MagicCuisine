e6a0b426da4bf8f9412792f382ef0ad8eb1ae28d
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-09-22 10:54:17 +0300

    Init app

---
+++ MagicCuisine/MagicCuisine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MagicCuisine.Startup))]
+namespace MagicCuisine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
