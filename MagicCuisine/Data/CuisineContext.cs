7e3dfd8f74e922f820183584aaabefcd20e1227f
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-09-22 16:41:01 +0300

    add dbcontext and test class

---
+++ MagicCuisine/Data/CuisineContext.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models;
+using System.Data.Entity;
+
+namespace Data
+{
+    public class CuisineContext : DbContext
+    {
+        public CuisineContext()
+                : base("CuisineDBConnection")
+        {
+        }
+
+        public IDbSet<Test> Tests { get; set; }
+    }
+}
---
+++ MagicCuisine/Data/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DataBase : IDataBase
+    {
+        private readonly CuisineContext context;
+
+        public DataBase(CuisineContext context)
+        {
+            this.context = context;
+        }
+
+        public int Complete()
+        {
+            return this.context.SaveChanges();
+        }
+    }
+}
