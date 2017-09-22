84cc7bfca28c8e3dbaf06b344f50128e731ce9b4
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-09-22 23:20:07 +0300

    add active link helper

---
+++ MagicCuisine/MagicCuisine/Helpers/LinkHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MagicCuisine.Helpers
+{
+    public static class LinkHelper
+    {
+        public static string IsSelected(this HtmlHelper html, string controllers = "", string actions = "", string cssClass = "selected")
+        {
+            ViewContext viewContext = html.ViewContext;
+            bool isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+            if (isChildAction)
+            {
+                viewContext = html.ViewContext.ParentActionViewContext;
+            }
+
+            RouteValueDictionary routeValues = viewContext.RouteData.Values;
+            string currentAction = routeValues["action"].ToString();
+            string currentController = routeValues["controller"].ToString();
+
+            if (String.IsNullOrEmpty(actions))
+            {
+                actions = currentAction;
+            }
+
+            if (String.IsNullOrEmpty(controllers))
+            {
+                controllers = currentController;
+            }
+
+            string[] acceptedActions = actions.ToLower().Trim().Split(',').Distinct().ToArray();
+            string[] acceptedControllers = controllers.ToLower().Trim().Split(',').Distinct().ToArray();
+
+            return acceptedActions.Contains(currentAction.ToLower()) && acceptedControllers.Contains(currentController.ToLower()) ?
+                cssClass : String.Empty;
+        }
+    }
+}
+
