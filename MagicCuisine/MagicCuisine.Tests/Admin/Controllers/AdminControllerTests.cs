c328da2ce19052138c72819a3bf55e22e4ed61d7
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-10-15 19:57:43 +0300

    add admin controller tests

---
+++ MagicCuisine/MagicCuisine.Tests/Admin/Controllers/AdminControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using MagicCuisine.Areas.Admin.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace MagicCuisine.Tests.Admin.Controllers
+{
+    [TestFixture]
+    public class AdminControllerTests
+    {
+        [Test]
+        public void IndexActionShould_ReturnNotNullViewResult()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void IndexActionShould_CallViewWithoutName()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+
+        [Test]
+        public void IndexActionShould_CallViewWithoutModel()
+        {
+            // Arrange
+            var controller = new AdminController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(null, result.Model);
+        }
+    }
+}
