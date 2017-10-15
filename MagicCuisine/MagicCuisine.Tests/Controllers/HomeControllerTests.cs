062f0ef7e0f842b159186dc0948a017dd070b725
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-10-15 23:41:09 +0300

    add home controller tests

---
+++ MagicCuisine/MagicCuisine.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,168 @@
+﻿using Data.Models;
+using MagicCuisine.Controllers;
+using MagicCuisine.Models;
+using MagicCuisine.Providers;
+using Moq;
+using NUnit.Framework;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MagicCuisine.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void ConstructorShould_ThrowArgumentNullException_WhenNullRecipeServiceIsPassedAsParameter()
+        {
+            //Arrange
+            var mapProvider = new Mock<IMapProvider>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new HomeController(null, mapProvider.Object));
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowArgumentNullException_WhenNullMapProviderIsPassedAsParameter()
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new HomeController(recipeService.Object, null));
+        }
+
+        [Test]
+        public void ConstructorShould_NotThrow_WhenValidArePassedAsParameters()
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+            var mapProvider = new Mock<IMapProvider>();
+
+            //Act & Assert
+            Assert.DoesNotThrow(() => new HomeController(recipeService.Object, mapProvider.Object));
+        }
+
+        [Test]
+        public void IndexShould_CallRecipeServiceMethodGetALL_WithFalseAsParameter()
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+            var mapProvider = new Mock<IMapProvider>();
+            var controller = new HomeController(recipeService.Object, mapProvider.Object);
+
+            var recipeList = new List<Recipe>();
+            recipeService.Setup(r => r.GetAll(It.IsAny<bool>())).Returns(recipeList);
+
+            //Act
+            controller.Index();
+
+            //Assert
+            recipeService.Verify(r => r.GetAll(false), Times.Once);
+        }
+
+        [Test]
+        public void IndexShould_CallMapProviderMethodGetMapOnce_WithRecipeAsParameter()
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+            var mapProvider = new Mock<IMapProvider>();
+            var controller = new HomeController(recipeService.Object, mapProvider.Object);
+
+            var recipe = new Recipe();
+            var recipeList = new List<Recipe>()
+            {
+                recipe
+            };
+            recipeService.Setup(r => r.GetAll(It.IsAny<bool>())).Returns(recipeList);
+
+            var recipeViewModel = new RecipeViewModel();
+            mapProvider.Setup(m => m.GetMap<RecipeViewModel>(It.IsAny<Object>())).Returns(recipeViewModel);
+            //Act
+            controller.Index();
+
+            //Assert
+            mapProvider.Verify(r => r.GetMap<RecipeViewModel>(recipe), Times.Once);
+        }
+
+        [Test]
+        public void IndexShould_ReturnView_WithoutViewName()
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+            var mapProvider = new Mock<IMapProvider>();
+            var controller = new HomeController(recipeService.Object, mapProvider.Object);
+
+            var recipe = new Recipe();
+            var recipeList = new List<Recipe>()
+            {
+                recipe
+            };
+            recipeService.Setup(r => r.GetAll(It.IsAny<bool>())).Returns(recipeList);
+
+            var recipeViewModel = new RecipeViewModel();
+            mapProvider.Setup(m => m.GetMap<RecipeViewModel>(It.IsAny<Object>())).Returns(recipeViewModel);
+            //Act
+            var result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+
+        [TestCase("Content/Images/general/logo.png")]
+        public void IndexShould_ReturnView_WithDefaultLogo(string expectedLogo)
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+            var mapProvider = new Mock<IMapProvider>();
+            var controller = new HomeController(recipeService.Object, mapProvider.Object);
+
+            var recipe = new Recipe();
+            var recipeList = new List<Recipe>()
+            {
+                recipe
+            };
+            recipeService.Setup(r => r.GetAll(It.IsAny<bool>())).Returns(recipeList);
+
+            var recipeViewModel = new RecipeViewModel();
+            mapProvider.Setup(m => m.GetMap<RecipeViewModel>(It.IsAny<Object>())).Returns(recipeViewModel);
+            //Act
+            var result = controller.Index() as ViewResult;
+            var model = result.Model as HomeIndexViewModel;
+
+            //Assert
+            Assert.AreEqual(expectedLogo, model.LogoImg);
+        }
+
+        [Test]
+        public void IndexShould_ReturnView_WithRecipeList()
+        {
+            //Arrange
+            var recipeService = new Mock<IRecipeService>();
+            var mapProvider = new Mock<IMapProvider>();
+            var controller = new HomeController(recipeService.Object, mapProvider.Object);
+
+            var recipe = new Recipe();
+            var recipeList = new List<Recipe>()
+            {
+                recipe
+            };
+            recipeService.Setup(r => r.GetAll(It.IsAny<bool>())).Returns(recipeList);
+
+            var recipeViewModel = new RecipeViewModel();
+            mapProvider.Setup(m => m.GetMap<RecipeViewModel>(It.IsAny<Object>())).Returns(recipeViewModel);
+            //Act
+            var result = controller.Index() as ViewResult;
+            var model = result.Model as HomeIndexViewModel;
+            var expecteList = new List<RecipeViewModel>()
+            {
+                recipeViewModel
+            };
+
+            //Assert
+            CollectionAssert.AreEqual(expecteList, model.Recipes);
+        }
+    }
+}
--- MagicCuisine/MagicCuisine/Controllers/HomeController.cs
+++ MagicCuisine/MagicCuisine/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
-﻿using AutoMapper;
-using MagicCuisine.Models;
+﻿using MagicCuisine.Models;
+using MagicCuisine.Providers;
 using Services.Contracts;
 using System;
 using System.Linq;
@@ -10,8 +10,9 @@ namespace MagicCuisine.Controllers
     public class HomeController : Controller
     {
         private readonly IRecipeService recipeService;
+        private readonly IMapProvider mapProvider;
 
-        public HomeController(IRecipeService recipeService)
+        public HomeController(IRecipeService recipeService, IMapProvider mapProvider)
         {
             if (recipeService == null)
             {
@@ -19,12 +20,19 @@ namespace MagicCuisine.Controllers
             }
 
             this.recipeService = recipeService;
+
+            if (mapProvider == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.mapProvider = mapProvider;
         }
 
         public ActionResult Index()
         {
             var recipes = this.recipeService.GetAll(false)
-                                            .Select(r => Mapper.Map<RecipeViewModel>(r))
+                                            .Select(r => this.mapProvider.GetMap<RecipeViewModel>(r))
                                             .ToList();
 
             var model = new HomeIndexViewModel()
